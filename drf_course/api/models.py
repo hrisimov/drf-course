d81f921f8f277ac4bad05e2453e29d3186af7d22
hrisimov <140465371+hrisimov@users.noreply.github.com>
2026-06-01 17:15:05 +0300

    Created `Product` model

--- drf_course/api/models.py
+++ drf_course/api/models.py
@@ -4,3 +4,31 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=200,
+    )
+
+    description = models.TextField()
+
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
+
+    stock = models.PositiveIntegerField()
+
+    image = models.ImageField(
+        upload_to='products/',
+        null=True,
+        blank=True,
+    )
+
+    @property
+    def in_stock(self):
+        return self.stock > 0
+
+    def __str__(self):
+        return self.name
