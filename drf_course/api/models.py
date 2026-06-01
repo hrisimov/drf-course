06a5f635ab893088fcf40aa91db782a256b99646
hrisimov <140465371+hrisimov@users.noreply.github.com>
2026-06-01 17:55:23 +0300

    Created `Order` model

--- drf_course/api/models.py
+++ drf_course/api/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -32,3 +34,33 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Order(models.Model):
+    class StatusChoices(models.TextChoices):
+        PENDING = 'Pending'
+        CONFIRMED = 'Confirmed'
+        CANCELLED = 'Cancelled'
+
+    order_id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+    )
+
+    status = models.CharField(
+        max_length=10,
+        choices=StatusChoices.choices,
+        default=StatusChoices.PENDING,
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return f'Order {self.order_id} by {self.user.username}'
