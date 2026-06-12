3a186fe21e77d637ab7df35c197a19b524a1279b
hrisimov <140465371+hrisimov@users.noreply.github.com>
2026-06-13 01:21:33 +0300

    Created `product_detail` function-based view & added to urls

--- drf_course/api/views.py
+++ drf_course/api/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -9,4 +10,11 @@ from api.serializers import ProductSerializer
 def product_list(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    serializer = ProductSerializer(product)
     return Response(serializer.data)
