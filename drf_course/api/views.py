82bdf823a9b51b1191ca6fe4982e33906b5083fd
hrisimov <140465371+hrisimov@users.noreply.github.com>
2026-06-17 02:59:28 +0300

    Created `order_list` function-based view & added to urls

--- drf_course/api/views.py
+++ drf_course/api/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from api.models import Product
-from api.serializers import ProductSerializer
+from api.models import Product, Order
+from api.serializers import ProductSerializer, OrderSerializer
 
 
 @api_view(['GET'])
@@ -17,4 +17,11 @@ def product_list(request):
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def order_list(request):
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many=True)
     return Response(serializer.data)
