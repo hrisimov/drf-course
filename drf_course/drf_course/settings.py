c022269016a4697d209b13ab0c25b8b452d2856a
hrisimov <140465371+hrisimov@users.noreply.github.com>
2026-07-29 19:11:26 +0300

    Installed and configured `django-silk` package

--- drf_course/drf_course/settings.py
+++ drf_course/drf_course/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django_extensions',
 
     'rest_framework',
+    'silk',
 
     'api'
 ]
@@ -40,6 +41,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'silk.middleware.SilkyMiddleware',
 ]
 
 ROOT_URLCONF = 'drf_course.urls'
--- drf_course/drf_course/urls.py
+++ drf_course/drf_course/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('silk/', include('silk.urls', namespace='silk')),
     path('', include('api.urls'))
 ]
