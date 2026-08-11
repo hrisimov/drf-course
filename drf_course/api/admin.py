1e6b7a93f45ba9ebf63e1d8df230360cf222bb63
hrisimov <140465371+hrisimov@users.noreply.github.com>
2026-08-11 21:35:42 +0300

    Registered `Order` model in admin site

--- drf_course/api/admin.py
+++ drf_course/api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from api.models import Order, OrderItem
+
+
+class OrderItemInLine(admin.TabularInline):
+    model = OrderItem
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderItemInLine,)
