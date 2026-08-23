ecc9c8fa95d498152f79f11691698e0554d534b9
hrisimov <140465371+hrisimov@users.noreply.github.com>
2026-08-23 18:57:53 +0300

    Created `UserOrdersTestCase` class with some test data

--- drf_course/api/tests.py
+++ drf_course/api/tests.py
@@ -1,3 +1,31 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+from api.models import Order
+
+UserModel = get_user_model()
+
+
+class UserOrdersTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user1 = UserModel.objects.create_user(
+            username='user1',
+            password='test',
+        )
+        cls.user2 = UserModel.objects.create_user(
+            username='user2',
+            password='test',
+        )
+        Order.objects.create(
+            user=cls.user1,
+        )
+        Order.objects.create(
+            user=cls.user1,
+        )
+        Order.objects.create(
+            user=cls.user2,
+        )
+        Order.objects.create(
+            user=cls.user2,
+        )
